c3f3595d39a74d32ed8111c5cbbebed245dcf008
AnjaliAlexx <anjalialex.mec@gmail.com>
2024-11-02 23:11:21 +0530

    Updated commit for QuizApp

---
+++ quizApp/README.md
@@ -0,0 +1,3 @@
+#Quiz Application
+
+An API to retrieve questions from postgreSQL database based on different scenarios 
---
+++ quizApp/src/main/java/com/sample/quizApp/dao/QuestionDAO.java
@@ -0,0 +1,12 @@
+package com.sample.quizApp.dao;
+
+import com.sample.quizApp.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionDAO extends JpaRepository<Question, Integer> {
+    List<Question> findByCategory(String category);
+}
